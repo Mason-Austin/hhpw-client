767d111f1e71b646012c7d4040f661cca61a232b
Mason Austin <mason47austin@gmail.com>
2024-01-16 18:40:09 -0600

    changed making a new user automotic no longer with a form

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,32 +1,19 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
 import PropTypes from 'prop-types';
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from 'react';
 import { registerUser } from '../utils/auth'; // Update with path to registerUser
 
 function RegisterForm({ user, updateUser }) {
   const [formData, setFormData] = useState({
-    bio: '',
+    name: user.fbUser.displayName,
     uid: user.uid,
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  useEffect(() => {
     registerUser(formData).then(() => updateUser(user.uid));
-  };
-
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
+  }, []);
+  return (null);
 }
 
 RegisterForm.propTypes = {
