154cbf50730c8aadc336942080148c0750e09759
Mason Austin <mason47austin@gmail.com>
2024-01-19 13:50:02 -0600

    completed sign in page. page welcomes user, and has 3 btns for viewing open orders, creating an order, and view total revenue

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
+import { useRouter } from 'next/router';
 import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
 
 function Home() {
   const { user } = useAuth();
+  const router = useRouter();
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
       style={{
         height: '90vh',
         padding: '30px',
-        maxWidth: '400px',
+        maxWidth: '600px',
         margin: '0 auto',
       }}
     >
       <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
+      <Button style={{ marginBottom: '3%' }} variant="danger" type="button" size="lg" className="copy-btn" onClick={() => { router.push('/orders/'); }}>
+        View Open Orders
+      </Button>
+      <Button style={{ marginBottom: '3%' }} variant="danger" type="button" size="lg" className="copy-btn" onClick={() => { router.push('/orders/new'); }}>
+        Create Order
+      </Button>
+      <Button style={{ marginBottom: '3%' }} variant="danger" type="button" size="lg" className="copy-btn" onClick={() => { router.push('/revenue'); }}>
+        View Total Revenue
       </Button>
     </div>
   );
